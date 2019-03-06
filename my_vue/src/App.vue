490f2beb0ea1184dea62cc66ee11ebba6cabba06
LgDevil <1412897049@qq.com>
2019-03-06 15:24:10 +0800

    first commit

---
+++ my_vue/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+      <el-container id="app">
+        <el-header class="header" height="100px">
+          <h3>我的网站</h3>
+          <p>Vue小模块之用户登录功能</p>
+        </el-header>
+        <el-main>
+        </el-main>
+      </el-container>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  .header{
+    background-color: #409EFF;
+    color: white;
+  }
+</style>
---
+++ my_vue/src/api/global.js
@@ -0,0 +1,7 @@
+
+const vue_host = "http://192.168.1.70:8067/vue_server/"
+
+
+
+
+export {vue_host}
